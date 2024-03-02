c24fb474cbbc4ee60684b1aaa437ce05491fb133
OatiesMilk <dylanakia2002@gmail.com>
2024-03-02 17:37:54 +0800

    update

--- app.js
+++ app.js
@@ -13,15 +13,22 @@ server.engine('hbs', handlebars.engine({
 
 server.use(express.static('public'));
 
-// Require a MongoDB connection. This will create a client
-// to connect to the specified mongoDB. The last part of the
-// URL is the database it connects to.
-const { MongoClient } = require('mongodb');
-const databaseURL = "mongodb://127.0.0.1:27017/";
-const mongoClient = new MongoClient(databaseURL);
+//Require a MongoDB connection using mongoose. Include the mongoose library
+//and feed it the correct url to run MongoDB.
+//URL is the database it connects to.
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/CCAPDEV');
 
-const databaseName = "CCAPDEV";
-const collectionName = "accounts";
+//Mongoose will need to define a schema that is used as a template.
+//This will contain the model details that is used by the schema.
+//the second array is for options. By default, Mongoose adds an extra
+//field for versioning. This will be removed.
+const accountsSchema = new mongoose.Schema({
+  user: { type: String },
+  pass: { type: String }
+}, { versionKey: false });
+
+const loginModel = mongoose.model('accounts', accountsSchema);
 
 function errorFn(err){
     console.log('Error found. Please trace!');
@@ -32,38 +39,45 @@ function successFn(res){
     console.log('Database query successful!');
 }
 
-mongoClient.connect().then(function(con){
-  console.log("Attempt to create!");
-  const dbo = mongoClient.db(databaseName);
-  dbo.createCollection(collectionName)
-    .then(successFn).catch(errorFn);
-}).catch(errorFn);
-
 server.get('/', function(req, resp){
 	resp.render('main',{
-        layout: 'index'
+        layout: 'index',
+        title: 'SulEAT Food Bites'
     });
 });
 
 server.post('/create-user', function(req, resp) {
-    const dbo = mongoClient.db(databaseName);
-    const col = dbo.collection(collectionName);
-
-    const info = {
+    //Creating a new instance can be made this way.
+    const loginInstance = loginModel({
         user: req.body.user,
         pass: req.body.pass
-    }
+    });
 
-    col.insertOne(info).then(function(res) {
-        console.log('Account created!');
-
-        resp.render('login', {
-            layout: 'index'
-        })
+    //to save this into the database, call the instance's save function.
+    //it will have a call-back to check if it worked.
+    loginInstance.save().then(function(accounts) {
+        console.log('Account created');
+        //change in interface is placed here as only when the query is
+        //successful do we update the interface.
+        resp.render('result',{
+        layout: 'index',
+        msg:  'Account created successfully'
+        });
     }).catch(errorFn);
 });
 
-const port = process.env.PORT | 9090;
+//Only at the very end should the database be closed.
+function finalClose(){
+    console.log('Connection closed!');
+    mongoose.connection.close();
+    process.exit();
+}
+
+process.on('SIGTERM',finalClose);  //general termination signal
+process.on('SIGINT',finalClose);   //catches when ctrl + c is used
+process.on('SIGQUIT', finalClose); //catches other termination commands
+
+const port = process.env.PORT | 3000;
 server.listen(port, function() {
     console.log('Listening at port '+port);
 });
